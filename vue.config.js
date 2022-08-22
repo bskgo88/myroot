880d2cc40a1a6e9431d72618b122b292408985e7
bskgo88 <bskgo88@gmail.com>
2022-08-22 11:59:19 +0900

    scss 환경설정

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,16 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+module.exports = {
+  css: {
+      loaderOptions: {
+          sass: {
+              data: `
+                  @import "@/assets/scss/custom.scss";
+                  @import "@/assets/scss/setting.scss";
+              `
+          }
+      }
+  }
+}
