ecfa2039a184747eaea0a042b5a87c861142a037
bskgo88 <bskgo88@gmail.com>
2022-09-19 15:32:05 +0900

    컴포넌트 분리

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="main">
+    <Header/>
     <img src="@/assets/images/main.png"/>
+    <Aside/>
+    <Footer/>
   </div>
 </template>
 
 <script>
+  //import Header from '@/include/header.vue'; // 헤더
+  import Footer from  '@/include/footer.vue'; // 푸터
+  import Aside from  '@/include/aside.vue'; // 메인 사이드 메뉴
+
 export default {
   name: 'MainPage',
   components: {
+    /*Header*/Footer, Aside
   }
 }
 </script>
--- src/include/header.vue
+++ src/include/header.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="header">
+			<ul>
+				<li><routerlink to="">파종회소개</routerlink></li>
+				<li><a href="#n">선원보감</a></li>
+				<li><a href="#n">인터넷족보</a></li>
+				<li><a href="#n">파종회소식</a></li>
+				<li><a href="#n">자료실</a></li>
+				<li><a href="#n">열린마당</a></li>
+			</ul>
   </div>
 </template>
 
