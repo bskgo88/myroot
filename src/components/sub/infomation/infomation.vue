65a5bf222ac000fed8636f12be829b8c0bedd5d4
bskgo88 <bskgo88@gmail.com>
2022-10-06 14:52:32 +0900

    신규파일 추가

---
+++ src/components/sub/infomation/infomation.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="subpage">
+    <div class="infoBox">
+
+		</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'infomationPage',
+  components: {
+  }
+}
+</script>
