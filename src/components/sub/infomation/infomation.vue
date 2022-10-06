d54ad8dd98a02dc548682f469dbf9a7d5a578633
bskgo88 <bskgo88@gmail.com>
2022-10-06 17:34:55 +0900

    scss 분리

--- src/components/sub/infomation/infomation.vue
+++ src/components/sub/infomation/infomation.vue
@@ -1,18 +1,27 @@
 <template>
   <div id="subpage">
+    <SubBox/>
     <div class="infoBox">
 		</div>
   </div>
 </template>
 
 <script>
+import SubBox from '@/components/sub/subtopBox.vue';
+
 export default {
   name: 'infomationPage',
   components: {
+    SubBox,
   },
 	data (){
     return {
 		}
 	},
+  computed: {
+    SubActive() {
+      return this.$route.meta.headerSub;
+    },
+  },
 }
 </script>
