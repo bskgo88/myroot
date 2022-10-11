3010fafe432dc65bf1c9a83c443a13941a1e689f
bskgo88 <bskgo88@gmail.com>
2022-10-11 13:30:40 +0900

    게시판 일괄 작업 완료

---
+++ src/components/sub/board/view.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="subpage" class="low">
+    <SubBox/>
+		<div class="MTView">
+			<div class="VHeader">
+				<strong class="VHText">제목이 들어가는 부분입니다. 제목이 들어가는 부분입니다.</strong>
+				<div class="VHDate">
+					<span>게시일 : 2022.10.11 13:16</span>
+					<span>조회수 : 125</span>
+				</div>
+			</div>
+			<div class="VText">
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+				<p>내용이 들어갑니다.</p>
+			</div>
+			<div class="VBtn">
+				<div class="VBLeft">
+					<router-link to="/notice">수정</router-link>
+					<router-link to="/notice">삭제</router-link>
+				</div>
+				<div class="VBRight"><router-link to="/notice">목록</router-link></div>
+			</div>
+		</div>
+  </div>
+</template>
+
+<script>
+	import SubBox from '@/components/sub/subtopBox.vue';
+
+	export default {
+		name: 'BoardViewPage',
+		components: {
+			SubBox,
+		},
+		data (){
+		return {
+				}
+		},
+	}
+</script>
