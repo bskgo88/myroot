14770e9f3550fefe9c6db5da6f8e6bee3d30125c
mimmi917 <mimmi.y917@gmail.com>
2022-10-14 14:56:59 +0900

    10.14

---
+++ src/components/AddGoogleMap.vue
@@ -0,0 +1,18 @@
+<template>
+    <GMapMap :center="center" :zoom="10" map-type-id="terrain" style="width: 100vw; height: 20rem">
+        <GMapCluster :zoomOnClick="true">
+            <GMapMarker :key="index" v-for="(m, index) in markers" :position="m.position" :clickable="true"
+                :draggable="true" @click="center = m.position" />
+        </GMapCluster>
+    </GMapMap>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 51.093048, lng: 6.84212 },
+    };
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router).mount('#app')
+app.use(router).mount('#app');
+
+import VueGoogleMaps from '@fawmi/vue-google-maps'
+app.use(VueGoogleMaps).mount('#app');
+
